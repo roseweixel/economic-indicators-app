270dfe783a420bd7d11dbfda5b1906b6a0c0f0c9
Rose Weixel <rose.weixel@gmail.com>
2015-02-04 17:52:43 -0500

    then and now toggle

--- app/controllers/now_categories_controller.rb
+++ app/controllers/now_categories_controller.rb
@@ -1,11 +1,12 @@
 class NowCategoriesController < ApplicationController
 
   def show
+    @year = params[:year]
     @categories = Category.all
     @category = Category.find(params[:id])
+    @line_item = LineItem.where(year: @year, category_id: @category.id).first
     @articles = @category.now_articles_hash.uniq {|a| a["headline"]["main"].downcase}
     respond_to do |format|
-      format.html
       format.js { }
     end
   end

0617e96f7f545d2d334a99141ae2af2d2d17bb6e
Rose Weixel <rose.weixel@gmail.com>
2015-02-03 23:36:39 -0500

    ajaxify line items to render on category show page

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,9 @@ class LineItemsController < ApplicationController
 
   def show
     @line_item = LineItem.where(category_id: params[:category_id], year: params[:id]).first
+    respond_to do |format|
+      format.js { }
+    end
   end
 
 end

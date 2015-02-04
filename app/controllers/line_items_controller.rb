268d53f7fbdb44276aa30d69abfb31fcef2a5c3a
changamanda <amanda.chang10@gmail.com>
2015-02-04 11:01:11 -0500

    Add NYT

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,8 @@ class LineItemsController < ApplicationController
 
   def show
     @line_item = LineItem.where(category_id: params[:category_id], year: params[:id]).first
+    @articles = @line_item.articles_hash
+    # @headlines = @line_item.headlines
     respond_to do |format|
       format.js { }
     end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,19 @@
 class LineItem < ActiveRecord::Base
   belongs_to :category
   validates_uniqueness_of :category_id, scope: :year
+
+  def articles_url
+  	"http://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{self.category.name}&begin_date=#{self.year}0101&end_date=#{self.year}1231&api-key=55fcb0439d641434b5b948714b22a6d5:7:71161329"
+  end
+
+  def articles_hash
+  	resp = Net::HTTP.get_response(URI.parse(self.articles_url))
+  	data = resp.body
+  	result = JSON.parse(data)
+  	result["response"]["docs"]
+  end
+
+  def headlines
+  	self.articles_hash.collect{ |article| article["headline"]["main"] }
+  end
 end
